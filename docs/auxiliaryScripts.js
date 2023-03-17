bee0722bcfda8d965bd6ab6ab395f5fc345289aa
Aryeh Zax <aryehzax@gmail.com>
2023-03-17 02:13:45 -0700

    pushing max date back to April 1st while I wait for the season 2 schedule

--- docs/auxiliaryScripts.js
+++ docs/auxiliaryScripts.js
@@ -14,7 +14,7 @@ $(function() {
     	minDate: new Date(2022, 11, 1),
     	defaultDate: new Date(),
     	setDate: new Date(),
-    	maxDate: new Date(2023, 2, 22),
+    	maxDate: new Date(2023, 3, 1),
     	onClose: function() {
     		var input = $(this).datepicker("getDate");
         	if (!input || !(input instanceof Date)) {
