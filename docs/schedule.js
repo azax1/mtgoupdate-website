24abdecda931c9f737f2fa8da77b2a4d90f691e4
Aryeh Zax <aryehzax@gmail.com>
2023-03-31 23:18:43 -0700

    adding missing let. oops

--- docs/schedule.js
+++ docs/schedule.js
@@ -30,7 +30,7 @@ function insertIntoMonsterSchedule(monster, today, specialEvents, isReplacement)
 		if (todayEvents) {
 			for (let j = 0; j < 24; j++) {
 				if (todayEvents[j] !== undefined) {
-					event = todayEvents[j];
+					let event = todayEvents[j];
 					index = 24 * i + j;
 					if (isReplacement) {
 						monster[index] = event;
