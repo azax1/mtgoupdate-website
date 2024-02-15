e46f3663ac979bbad3898164c423c48af306cc89
Aryeh Zax <aryehzax@gmail.com>
2024-02-15 10:36:05 -0800

    fixing bug where when multiple events fire at midnight, only the first one gets copied to the prior day's schedule

--- docs/displayWebpage.js
+++ docs/displayWebpage.js
@@ -184,16 +184,20 @@ function getSchedulesForWeek(
     let sched = schedules[today.getDate()];
     if (sched) {
       ret[i] = sched.map((x) => x[0].replace("#EVENT", addTooltip(x[1])).replace("#COLOR", x[2])).join("");
-      let event = sched[0];
-      if (event[0].includes("12am") || event[0].includes("00:00")) {
-        ret[i - 1] += event[0]
-          .replace("#EVENT", addTooltip(event[1]))
-          .replace("12am", "Midnight")
-          .replace("00:", "24:")
-          .replace(
-            "#COLOR",
-            getEventColor(event[1], isDarkMode, true, i === 8)
-          );
+      for (let j = 0; j < sched.length; j++) {
+        let event = sched[j];
+        if (event[0].includes("12am") || event[0].includes("00:00")) {
+          ret[i - 1] += event[0]
+            .replace("#EVENT", addTooltip(event[1]))
+            .replace("12am", "Midnight")
+            .replace("00:", "24:")
+            .replace(
+              "#COLOR",
+              getEventColor(event[1], isDarkMode, true, i === 8)
+            );
+        } else {
+          break;
+        }
       }
     }
     today.setDate(today.getDate() + 1);
