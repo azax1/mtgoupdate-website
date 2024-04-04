a2982505f038e0db55df24bb1f191337a1698335
Aryeh Zax <aryehzax@gmail.com>
2024-04-04 12:47:22 -0400

    new Vintage Challenge

--- docs/schedule.js
+++ docs/schedule.js
@@ -62,7 +62,7 @@ function getBaseSchedule() {
 		`Modern ${pr}`, null, null, `Pioneer ${pr}`, null, null, null, `Limited ${pr}`, null, null, `Standard ${pr}`, `Modern ${pr}`,
 		`Limited ${pr}`, null, null, `Legacy ${pr}`, `Limited ${pr}`, `Standard ${pr}`, null, `Modern ${pr}`, null, `Pioneer ${pr}`, null, null,
 		`Limited ${pr}`, null, `Limited ${pr}`, null, `Modern ${pr}`, null, null, `Legacy ${pr}`, null, `Standard ${pr}`, `Limited ${pr}`, `Vintage ${pr}`,
-		null, null, `Pioneer ${ch}`, `Standard ${ch} (32-player)`, `Limited ${ch} (32-player)`, null, `Modern ${ch}`, `Standard ${pr}`, null, null, null, `Modern ${ch} (32-player)`,
+		null, null, `Pioneer ${ch}`, `Standard ${ch} (32-player)&Vintage ${ch} (32-player)`, `Limited ${ch} (32-player)`, null, `Modern ${ch}`, `Standard ${pr}`, null, null, null, `Modern ${ch} (32-player)`,
 		`Pioneer ${pr}`, `Limited ${pr}`, `Pauper ${ch}`, `Limited ${ch} (32-player)`, `Legacy ${ch} (32-player)`, null, `Standard ${ch} (32-player)`, null, `Modern ${ch} (96-player)&plus&Modern ${ch}&minus`, null, `Vintage ${ch}`, null,
 		`Limited ${ch} (32-player)`, null, `Pioneer ${ch}`, null, `Limited ${pr}`, null, `Modern ${ch}`, null, `Legacy ${ch} (32-player)`, null, null, null];
 	return ret;
