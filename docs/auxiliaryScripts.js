5210e55168664195672f24b0eb0a5ceba62f15e5
Aryeh Zax <aryehzax@gmail.com>
2024-07-15 13:09:08 -0700

    season 3 changes

--- docs/auxiliaryScripts.js
+++ docs/auxiliaryScripts.js
@@ -59,10 +59,10 @@ $(function () {
   $("#datepicker")
     .datepicker({
       dateFormat: "yy-mm-dd",
-      minDate: new Date(2023, 10, 26),
+      minDate: new Date(2024, 6, 1),
       defaultDate: new Date(),
       setDate: new Date(),
-      maxDate: new Date(2024, 6, 31),
+      maxDate: new Date(2024, 10, 30),
       onClose: function () {
         var input = $(this).datepicker("getDate");
         if (!input || !(input instanceof Date)) {
--- docs/scheduleData.js
+++ docs/scheduleData.js
@@ -34,87 +34,73 @@ function getBaseSchedule() {
 
 function getRCQData() {
     return [
-            [2024, 2, 23, {8: "Modern Showcase Qualifier (invite-only)"}],
-            [2024, 2, 24, {6: "Pioneer Showcase Qualifier (invite-only)"}],
-            [2024, 2, 30, {10: "Vintage Showcase Qualifier (invite-only)"}],
-            [2024, 2, 31, {8: "Legacy Showcase Qualifier (invite-only)"}],
-            [2024, 3, 6, {7: "Limited (MKM) MOCS Showcase Open"}],
-            [2024, 3, 7, {7: "Limited (MKM) MOCS Showcase Open"}],
-            [2024, 3, 12, {14: "Limited (MKM) Qualifier"}],
-            [2024, 3, 27, {7: "Limited (OTJ) Super Qualifier"}],
-            [2024, 3, 28, {7: "Standard Super Qualifier"}],
-            [2024, 4, 1, {5: "Modern Qualifier"}],
-            [2024, 4, 4, {7: "Pioneer Super Qualifier"}],
-            [2024, 4, 5, {7: "Vintage Qualifier"}],
-            [2024, 4, 19, {7: "Modern Super Qualifier"}],
-            [2024, 4, 17, {14: "Pioneer Super Qualifier"}],
-            [2024, 4, 27, {7: "Limited (OTJ) Qualifier"}],
-            [2024, 4, 31, {14: "Legacy Qualifier"}],
-            [2024, 5, 1, {1: "Limited (OTJ) Qualifier"}],
-            [2024, 5, 2, {1: "Legacy Qualifier"}],
-            [2024, 5, 8, {7: "Pauper Super Qualifier"}],
-            [2024, 5, 9, {7: "Limited (OTJ) Super Qualifier"}],
-            [2024, 5, 16, {7: "Limited (MH3) Super Qualifier"}],
-            [2024, 5, 21, {7: "Limited (MH3) Qualifier"}],
-            [2024, 5, 29, {7: "Limited (MH3) Super Qualifier"}],
-            [2024, 5, 30, {7: "Modern Super Qualifier"}],
-            [2024, 6, 12, {7: "Limited (MH3) Super Qualifier"}],
-            [2024, 6, 13, {8: "Modern Showcase Qualifier (invite-only)"}],
-            [2024, 6, 14, {6: "Pioneer Showcase Qualifier (invite-only)", 12: "Modern Super Qualifier"}],
-            [2024, 6, 20, {8: "Pauper Showcase Qualifier (invite-only)"}],
-            [2024, 6, 21, {8: "Legacy Showcase Qualifier (invite-only)"}],
-            [2024, 6, 27, {7: "Limited (MH3) MOCS Showcase Open"}],
-            [2024, 6, 28, {7: "Limited (MH3) MOCS Showcase Open"}],
-    ];
+            [6, 13, {8: "Modern Showcase Qualifier (invite-only)"}],
+            [6, 14, {6: "Pioneer Showcase Qualifier (invite-only)", 12: "Modern Super Qualifier"}],
+            [6, 20, {8: "Pauper Showcase Qualifier (invite-only)"}],
+            [6, 21, {8: "Legacy Showcase Qualifier (invite-only)"}],
+            [6, 27, {7: "Limited (MH3) MOCS Showcase Open"}],
+            [6, 28, {7: "Limited (MH3) MOCS Showcase Open"}],
+            [7, 26, {7: "Limited (MH3) Super Qualifier"}],
+            [8, 4, {7: "Modern Super Qualifier"}],
+            [8, 11, {7: "Limited (BLB) Super Qualifier"}],
+            [8, 23, {14: "Limited (BLB) Super Qualifier"}],
+            [8, 30, {14: "Modern Super Qualifier"}],
+            [8, 31, {7: "Vintage Qualifier"}],
+            [9, 1, {7: "Limited (BLB) Super Qualifier"}],
+            [9, 2, {7: "Pioneer Qualifier"}],
+            [9, 7, {7: "Limited (BLB) Qualifier"}],
+            [9, 8, {7: "Modern Super Qualifier"}],
+            [9, 14, {7: "Limited (BLB) Super Qualifier"}],
+            [9, 15, {7: "Standard Super Qualifier"}],
+            [9, 29, {7: "Pioneer Super Qualifier"}],
+            [10, 5, {1: "Limited (DSK) Super Qualifier"}],
+            [10, 6, {7: "Legacy Super Qualifier"}],
+            [10, 12, {7: "Pauper Qualifier"}],
+            [10, 13, {7: "Limited (DSK) Super Qualifier"}],
+            [10, 25, {14: "Limited (DSK) Qualifier"}],
+            [10, 26, {1: "Limited (DSK) Qualifier"}],
+            [10, 27, {1: "Standard Qualifier"}],
+    ].map(supplyYearAndDecrementMonth);
 }
 
 function getShowcaseData() {
     return [
-            [2024, 3, 13, {8: "Modern Showcase Challenge"}],
-            [2024, 3, 14, {4: "Modern Challenge", 6: "Pioneer Showcase Challenge"}],
-            [2024, 3, 20, {7: "Pauper Showcase Challenge"}],
-            [2024, 3, 21, {4: "Modern Challenge", 8: "Legacy Showcase Challenge"}],
-            [2024, 4, 11, {8: "Modern Showcase Challenge"}],
-            [2024, 4, 12, {4: "Modern Challenge", 6: "Pioneer Showcase Challenge"}],
-            [2024, 4, 25, {7: "Pauper Showcase Challenge"}],
-            [2024, 4, 26, {4: "Modern Challenge", 8: "Legacy Showcase Challenge"}],
-            [2024, 5, 22, {8: "Modern Showcase Challenge"}],
-            [2024, 5, 23, {4: "Modern Challenge", 6: "Pioneer Showcase Challenge"}],
-            [2024, 6, 6, {8: "Pauper Showcase Challenge"}]
-    ];
+            [8, 17, {8: "Modern Showcase Challenge"}],
+            [8, 18, {6: "Pioneer Showcase Challenge"}],
+            [8, 24, {6: "Standard Showcase Challenge"}],
+            [8, 25, {8: "Legacy Showcase Challenge"}],
+            [9, 22, {8: "Legacy Showcase Challenge"}],
+            [10, 19, {6: "Standard Showcase Challenge"}],
+            [10, 20, {6: "Pioneer Showcase Challenge"}],
+            [11, 2, {8: "Modern Showcase Challenge"}],
+            [11, 16, {8: "Modern Showcase Challenge"}],
+            [11, 17, {6: "Pioneer Showcase Challenge"}],
+            [11, 23, {6: "Standard Showcase Challenge"}],
+            [11, 24, {8: "Legacy Showcase Challenge"}],
+    ].map(supplyYearAndDecrementMonth);
 }
 
 function getHackishLCQCorrections() {
     return [
-        [2024, 6, 7, 4, "Modern Challenge"],
-        [2024, 6, 7, 8, "Legacy Showcase Challenge"],
-        [2024, 6, 7, 9, null],
-        [2024, 6, 7, 11, "Modern LCQ"],
-        [2024, 6, 8, 15, "Modern LCQ&plus&Pauper LCQ&minus"],
-        [2024, 6, 9, 11, null],
-        [2024, 6, 9, 12, "plus&Legacy LCQ&minus"],
-        [2024, 6, 9, 16, null],
-        [2024, 6, 9, 18, "Pauper LCQ"],
-        [2024, 6, 10, 2, null],
-        [2024, 6, 10, 5, "Pauper LCQ"]
-    ];
+        [11, 24, 8, "Legacy Showcase Challenge"],
+    ].map(supplyYearAndDecrementMonth);
 }
 
 function getCubeEvents() {
     return [
             [
-                [ [2024, 6, 2] ],
+                [ [6, 2] ],
                 [ 12.5, 14.5, 16.5, 18.5, 20.5, 22.5 ]
             ],
             [
-                [ [2024, 6, 3], [2024, 6, 4], [2024, 6, 5], [2024, 6, 6], [2024, 6, 7], [2024, 6, 8] ],
+                [ [6, 3], [6, 4], [6, 5], [6, 6], [6, 7], [6, 8] ],
                 [ 0.5, 2.5, 4.5, 6.5, 8.5, 10.5, 12.5, 14.5, 16.5, 18.5, 20.5, 22.5 ],
             ],
             [
-                [ [2024, 6, 9] ],
+                [ [6, 9] ],
                 [ 0.5, 2.5, 4.5, 6.5, 8.5, 10.5, 12.5, 14.5, 16.5, 18.5 ],
             ]
-        ];
+        ].map(([dates, times]) => [dates.map(supplyYearAndDecrementMonth), times]);
 }
 
 function getInferredYear(today, proposedMonth) {
@@ -128,6 +114,10 @@ function getInferredYear(today, proposedMonth) {
     return currentYear;
 }
 
+function supplyYearAndDecrementMonth(data) {
+    return [getInferredYear(new Date(), data[0]), data[0] - 1, ...data.slice(1)];
+}
+
 function getCurrentYearDSTDates() {
 	let year = new Date().getFullYear();
 	let march = new Date(year, 2, 8 + ((7 - new Date(year, 2, 1).getDay()) % 7));
@@ -136,5 +126,5 @@ function getCurrentYearDSTDates() {
 }
 
 function getLCQStartDate() {
-    return [2024, 10, 24];
+    return supplyYearAndDecrementMonth([11, 24]);
 }
