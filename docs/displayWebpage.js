ceb54984331947241d025ad7a90d01ef960087ce
Aryeh Zax <aryehzax@gmail.com>
2024-08-09 22:44:55 -0700

    adding more shilling

--- docs/displayWebpage.js
+++ docs/displayWebpage.js
@@ -52,7 +52,7 @@ function displaySchedule(fixSliders = true) {
     // `;
     document.getElementById("banner").innerHTML = `
       <center>
-      mtgoupdate has a Patreon! <a href="https://www.patreon.com/mtgoupdate">Click here</a> to become a patron.
+      Thanks for using mtgoupdate! Please consider supporting this project via <a href="https://www.patreon.com/mtgoupdate">Patreon</a> or <a href="https://ko-fi.com/mtgoupdate">ko-fi</a>.
       </center>
       `;
   }
