3f45f3c4e668999174663d40f0704cbf62811bf3
Aryeh Zax <aryehzax@gmail.com>
2025-03-12 13:41:24 -0400

    replacing Pioneer Prelims with Commander Trials. fuck Ben Weitz

--- docs/scheduleData.js
+++ docs/scheduleData.js
@@ -6,19 +6,19 @@ function getBaseSchedule() {
 		`Limited ${ch} (32-player)`, `Pioneer ${ch}`, `Standard ${ch}`, null, `Commander Trial (16-player)`, `Limited ${pr}`,
         `Modern ${ch} (32-player)`, null, null, null, null, null];
     const mon = [`Pioneer ${pr}`, null, null, `Limited ${pr}`, `Legacy ${pr}`, `Standard ${pr}`,
-        null, `Modern ${ch}`, `Pioneer ${pr}`, `Limited ${pr}`, null, `Legacy ${pr}`,
+        null, `Modern ${ch}`, `Commander Trial (16-player)`, `Limited ${pr}`, null, `Legacy ${pr}`,
 		null, `Standard ${ch}`, null, `Modern ${pr}`, `Pioneer ${ch} (32-player)`, `Vintage ${pr}`,
         `Limited ${pr}`, `Pioneer ${pr}`, null, `Modern ${pr}`, null, null];
     const tues = [`Limited ${pr}`, null, null, `Modern ${pr}`, null, null,
         null, `Pioneer ${pr}`, null, `Limited ${pr}`, `Standard ${ch}`, `Modern ${pr}`,
-		null, `Modern ${ch} (64-player)`, null, `Pioneer ${pr}`, `Limited ${pr}`, `Modern ${pr}`,
+		null, `Modern ${ch} (64-player)`, null, `Commander Trial (16-player)`, `Limited ${pr}`, `Modern ${pr}`,
         `Standard ${pr}`, `Legacy ${pr}`, null, null, null, null];
     const wed = [`Limited ${pr}`, null, `Modern ${pr}`, `Legacy ${pr}`, null, null,
-        `Pioneer ${pr}`, `Limited ${pr}`, `Standard ${pr}`, `Modern ${ch}`, null, `Limited ${pr}`,
-		`Standard ${pr}`, `Pioneer ${pr}`, `Legacy ${ch} (32-player)`, `Modern ${pr}`, `Limited ${pr}`, `Standard ${ch} (32-player)`,
+        `Commander Trial (16-player)`, `Limited ${pr}`, `Standard ${pr}`, `Modern ${ch}`, null, `Limited ${pr}`,
+		`Standard ${pr}`, `Commander Trial (16-player)`, `Legacy ${ch} (32-player)`, `Modern ${pr}`, `Limited ${pr}`, `Standard ${ch} (32-player)`,
         `Vintage ${pr}`, null, null, `Limited ${pr}`, null, null];
     const thur = [`Modern ${pr}`, null, null, `Pioneer ${pr}`, null, `Standard ${pr}`,
-        `Modern ${ch}`, `Limited ${pr}`, `Vintage ${ch} (32-player)`, `Pioneer ${pr}`, `Standard ${pr}`, `Modern ${pr}`,
+        `Modern ${ch}`, `Limited ${pr}`, `Vintage ${ch} (32-player)`, `Commander Trial (16-player)`, `Standard ${pr}`, `Modern ${pr}`,
 		`Limited ${pr}`, null, `Pioneer ${ch}`, `Legacy ${pr}`, `Limited ${pr}`, `Modern ${ch}`,
         null, `Modern ${pr}`, null, `Pioneer ${pr}`, null, null];
     const fri = [`Modern ${pr}`, null, `Limited ${pr}`, null, `Modern ${pr}`, `Pioneer ${ch} (32-player)`,
