ea3c9e7f26cc84d10239eadbb9f37a6661bd2981
Aryeh Zax <aryehzax@gmail.com>
2025-04-29 22:40:11 -0700

    schedule changes

--- docs/scheduleData.js
+++ docs/scheduleData.js
@@ -7,28 +7,28 @@ function getBaseSchedule() {
         `Pioneer ${ch} (32-player)`, null, `Legacy ${ch} (32-player)`, `Modern ${pr}`, `Pauper ${ch} (32-player)`, null,
 		`Limited ${ch} (32-player)`, `Pioneer ${ch} (32-player)`, `Standard ${ch}`, null, `Commander ${tr} (16-player)`, `Limited ${pr}`,
         `Modern ${ch} (32-player)`, null, null, null, null, null];
-    const mon = [`Pioneer ${pr}`, null, null, `Limited ${pr}`, `Legacy ${pr}`, `Standard ${pr}`,
+    const mon = [`Pioneer ${thr}`, null, null, `Limited ${pr}`, `Legacy ${pr}`, `Standard ${pr}`,
         null, `Modern ${ch}`, `Commander ${tr} (16-player)`, `Limited ${pr}`, null, `Legacy ${pr}`,
-		null, `Standard ${ch}`, `Pauper ${thr}`, `Modern ${pr}`, `Pioneer ${ch} (32-player)`, `Vintage ${pr}`,
-        `Limited ${pr}`, `Pioneer ${pr}`, null, `Modern ${pr}`, null, null];
+		null, `Standard ${ch}`, `Pauper ${thr}`, `Modern ${pr}`, `Pioneer ${ch} (32-player)`, `Vintage ${thr}`,
+        `Limited ${pr}`, `Pioneer ${thr}`, null, `Modern ${pr}`, null, null];
     const tues = [`Limited ${pr}`, null, null, `Modern ${pr}`, null, null,
         `Pauper ${thr}`, null, `Phantom Sealed ${tr} (24-player)`, null, `Standard ${ch}`, null,
 		null, `Modern ${ch} (32-player)`, null, `Commander ${tr} (16-player)`, `Phantom Sealed ${tr} (24-player)`, `Modern ${pr}`,
         `Standard ${pr}`, `Legacy ${pr}`, null, null, null, null];
     const wed = [`Limited ${pr}`, null, `Modern ${pr}`, `Legacy ${pr}`, null, null,
         `Commander ${tr} (16-player)`, `Limited ${pr}`, `Standard ${pr}`, `Modern ${ch}`, null, `Limited ${pr}`,
-		`Standard ${pr}`, `Commander ${tr} (16-player)`, `Legacy ${ch} (32-player)`, `Pauper ${thr}`, `Phantom Sealed ${tr} (24-player)`, `Standard ${ch} (32-player)`,
+		`Standard ${pr}`, `Commander ${tr} (16-player)`, `Legacy ${ch} (32-player)`, `Pauper ${thr}`, `Phantom Sealed ${tr} (24-player)`, `Standard ${ch}`,
         null, null, null, `Limited ${pr}`, null, null];
-    const thur = [`Modern ${pr}`, null, null, `Pioneer ${pr}`, null, `Standard ${pr}`,
+    const thur = [`Modern ${pr}`, null, null, `Pioneer ${thr}`, null, `Standard ${pr}`,
         `Modern ${ch}`, `Limited ${pr}`, `Vintage ${ch} (32-player)`, `Commander ${tr} (16-player)`, `Pauper ${thr}`, `Standard ${pr}`,
-		`Phantom Sealed ${tr} (24-player)`, `Modern ${pr}`, `Pioneer ${ch} (32-player)`, `Legacy ${pr}`, `Limited ${pr}`, `Standard ${ch} (32-player)`,
-        null, `Modern ${pr}`, null, `Pioneer ${pr}`, null, null];
+		`Phantom Sealed ${tr} (24-player)`, `Modern ${pr}`, `Pioneer ${ch} (32-player)`, `Legacy ${pr}`, `Limited ${pr}`, `Standard ${ch}`,
+        null, `Modern ${pr}`, null, `Pioneer ${thr}`, null, null];
     const fri = [`Modern ${pr}`, null, `Limited ${pr}`, null, `Modern ${pr}`, `Pioneer ${ch} (32-player)`,
-        `Pauper ${ch} (32-player)`, `Legacy ${pr}`, `Modern ${ch}`, `Limited ${pr}`, `Standard ${ch} (32-player)`, `Vintage ${pr}`,
+        `Pauper ${ch} (32-player)`, `Legacy ${pr}`, `Modern ${ch}`, `Limited ${pr}`, `Standard ${ch}`, `Vintage ${thr}`,
 		`Commander ${tr} (16-player)`, `Pauper ${thr}`, `Pioneer ${ch} (32-player)`, `Vintage ${ch} (32-player)`, `Limited ${ch} (32-player)`, `Legacy ${ch} (32-player)`,
         `Modern ${ch} (32-player)`, `Standard ${ch} (32-player)`, null, null, null, `Modern ${ch} (32-player)`];
-    const sat = [`Pioneer ${pr}`, `Phantom Sealed ${tr} (24-player)`, `Pauper ${ch} (32-player)`, null, `Legacy ${ch} (32-player)`, null,
-        `Standard ${ch} (32-player)`, null, `plus&Modern ${ch}&minus`, `Commander ${tr} (16-player)`, `Vintage ${ch} (32-player)`, null,
+    const sat = [`Pioneer ${thr}`, `Phantom Sealed ${tr} (24-player)`, `Pauper ${ch} (32-player)`, null, `Legacy ${ch} (32-player)`, null,
+        `Standard ${ch}`, null, `plus&Modern ${ch}&minus`, `Commander ${tr} (16-player)`, `Vintage ${ch} (32-player)`, null,
 		`Limited ${ch} (32-player)`, null, `Pioneer ${ch}`, null, `Limited ${pr}`, null,
         `Modern ${ch} (32-player)`, null, `Legacy ${ch} (32-player)`, null, null, null];
 	return sun.concat(mon).concat(tues).concat(wed).concat(thur).concat(fri).concat(sat);
