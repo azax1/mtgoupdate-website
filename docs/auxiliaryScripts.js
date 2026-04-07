08cc37b429093cf250885b01d802830c95082558
Aryeh Zax <aryehzax@gmail.com>
2026-04-06 20:30:46 -0400

    forgot to change the allowed date range oops

--- docs/auxiliaryScripts.js
+++ docs/auxiliaryScripts.js
@@ -59,10 +59,10 @@ $(function () {
   $("#datepicker")
     .datepicker({
       dateFormat: "yy-mm-dd",
-      minDate: new Date(2025, 11, 1),
+      minDate: new Date(2026, 3, 1),
       defaultDate: new Date(),
       setDate: new Date(),
-      maxDate: new Date(2026, 3, 30),
+      maxDate: new Date(2026, 8, 31),
       onClose: function () {
         var input = $(this).datepicker("getDate");
         if (!input || !(input instanceof Date)) {
